7ab404b0b8529e7c148b0b3116757f7f5d8ee53b
kaffelmedhoussem67 <kaffelhoussem067@gmail.com>
2024-01-02 16:36:44 +0100

    Task-04 - Implementing Animal Apis + enchance code

--- src/main/java/com/example/apiproject/controller/AnimalController.java
+++ src/main/java/com/example/apiproject/controller/AnimalController.java
@@ -1,18 +1,62 @@
 package com.example.apiproject.controller;
 
 
+import com.example.apiproject.dto.AnimalUpdateRequest;
+import com.example.apiproject.entity.Animal;
+import com.example.apiproject.exceptions.ResourceNotFoundException;
+import com.example.apiproject.service.AnimalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/test")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/animal")
 public class AnimalController {
 
+    private final AnimalService animalService;
     @GetMapping
-    public ResponseEntity<String> test(){
-        return ResponseEntity.ok("Hello from secured endpoint");
+    public ResponseEntity<List<Animal>> getAllAnimals(){
+        var animals = animalService.getAllAnimals();
+        return new ResponseEntity<>(animals, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Animal> create(@RequestBody Animal animal, @RequestHeader("Authorization") String token){
+        var animalToCreate=animalService.create(animal,token);
+        return new ResponseEntity<>(animalToCreate,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable Integer id, @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(animalService.delete(id,token));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Animal> getAnimal(@PathVariable Integer id){
+        try {
+            var animal=animalService.showAnimalDetails(id);
+            return new ResponseEntity<>(animal,HttpStatus.OK);
+        }catch (ResourceNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+
+
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<String> updateAnimalDescription( @PathVariable Integer id,@RequestBody AnimalUpdateRequest request,@RequestHeader("Authorization")String token) {
+        try {
+            animalService.update(id,token,request );
+            return ResponseEntity.ok("Animal description updated successfully");
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
